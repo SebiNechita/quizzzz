8a67c9782c15c72de008a33365826d7f41dd6fc2
Sander Vermeulen <S.R.Vermeulen@student.tudelft.nl>
2022-02-23 16:05:14 +0100

    Added some test templates

---
+++ client/src/test/java/client/scenes/MainCtrlTest.java
@@ -0,0 +1,20 @@
+package client.scenes;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MainCtrlTest {
+
+    private MainCtrl mainCtrl;
+
+    @BeforeEach
+    public void setup() {
+        mainCtrl = new MainCtrl();
+    }
+
+    @Test
+    public void writeSomeTests() {
+        // TODO create replacement objects and write some tests
+        // sut.initialize(null, null, null);
+    }
+}
---
+++ server/src/test/java/server/api/SomeControllerTest.java
@@ -0,0 +1,17 @@
+package server.api;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class SomeControllerTest {
+
+    @BeforeEach
+    public void setup() {
+    }
+
+    @Test
+    public void someTest() {
+
+    }
+
+}
