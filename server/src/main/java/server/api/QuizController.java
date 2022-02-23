94c40341d2c919a090f3cf88207ef5d9b5181b70
Kristof Levay <levaykristof@student.tudelft.nl>
2022-02-23 12:16:07 +0100

    Add Javadoc to greeting endpoint.

--- server/src/main/java/server/api/QuizController.java
+++ server/src/main/java/server/api/QuizController.java
@@ -1,19 +1,17 @@
 package server.api;
-import commons.Quote;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/api")
 public class QuizController {
+    /**
+     * Responds with the name defined in the path.
+     */
     @GetMapping(path = { "/greet/{name}"})
     public String getGreeting(@PathVariable("name") String name) {
         return "Hello, " + name + "!";
     }
-
 }
