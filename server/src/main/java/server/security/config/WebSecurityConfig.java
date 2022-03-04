b9e59fd831da71252eaad38f439b37823ca98903
Sander Vermeulen <S.R.Vermeulen@student.tudelft.nl>
2022-03-04 12:15:13 +0100

    Allow h2-console to bypass authentication

--- server/src/main/java/server/security/config/WebSecurityConfig.java
+++ server/src/main/java/server/security/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -32,13 +33,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                // Allow the h2-console to bypass HTTPS and authentication
-                .authorizeRequests()
-                .antMatchers("/h2-console", "/api/register")
-                .permitAll()
-                .and()
-
-                // Force all other traffic to use HTTPS
                 .requiresChannel(channel -> channel.anyRequest().requiresSecure())
                 .authorizeRequests()
                 .antMatchers("/api/registration/**")
@@ -54,6 +48,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .anyRequest()
                 .authenticated();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/h2-console/**");
     }
 
     @Override
