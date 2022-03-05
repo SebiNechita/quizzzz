f085668596b6d3939eb3323d3815bfcd3bbca276
Kristof Levay <levaykristof@student.tudelft.nl>
2022-03-05 13:42:37 +0100

    Remove unused imports and whitespace

--- server/src/main/java/server/api/GreetingController.java
+++ server/src/main/java/server/api/GreetingController.java
@@ -1,20 +1,10 @@
 package server.api;
 
-import commons.LeaderboardEntry;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import server.database.LeaderboardRepository;
-
-import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api")
 public class GreetingController {
-
-
-
-
-
     @GetMapping(path = "/greet/{name}")
     public String sendGreeting(@PathVariable("name") String name) {
         return "Hi, " + name + "!";
