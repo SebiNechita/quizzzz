0c82a2c1e77eb8247f71cced135572ca68ac1242
Sander Vermeulen <S.R.Vermeulen@student.tudelft.nl>
2022-03-09 13:58:48 +0100

    Refactor for consistency
    The URLs for the user management (register, login, available, delete) have now all been put into the same /api/user endpoints). Packets have also been slightly refactored to make everything more in one line.

--- commons/src/main/java/packets/DeleteResponsePacket.java
+++ commons/src/main/java/packets/DeleteResponsePacket.java
@@ -2,24 +2,72 @@ package packets;
 
 import commons.utils.HttpStatus;
 
-public class DeleteResponsePacket extends RegisterResponsePacket{
-
-    public DeleteResponsePacket() {
+public class DeleteResponsePacket extends GeneralResponsePacket {
+    /**
+     * Required for object mappers
+     */
+    protected DeleteResponsePacket() {
     }
 
-    public DeleteResponsePacket(int code) {
-        super(code);
+    /**
+     * Constructor for this packet
+     *
+     * @param httpCode The HTTP code to send to the receiver of this packet
+     */
+    public DeleteResponsePacket(int httpCode) {
+        super(httpCode);
     }
 
+    /**
+     * Constructor for this packet
+     *
+     * @param status The HTTP status to send to the receiver of this packet
+     */
     public DeleteResponsePacket(HttpStatus status) {
         super(status);
     }
 
-    public DeleteResponsePacket(int code, String message) {
-        super(code, message);
+    /**
+     * Constructor for this packet
+     *
+     * @param httpCode The HTTP code to send to the receiver of this packet
+     * @param message  The message to send to the receiver of this packet
+     */
+    public DeleteResponsePacket(int httpCode, String message) {
+        super(httpCode, message);
     }
 
+    /**
+     * Constructor for this packet
+     *
+     * @param status  The HTTP status to send to the receiver of this packet
+     * @param message The message to send to the receiver of this packet
+     */
     public DeleteResponsePacket(HttpStatus status, String message) {
         super(status, message);
+    }
+
+    /**
+     * Checks if this packet is equal to the object specified as a parameter
+     *
+     * @param other The other object to compare this with
+     * @return If they are equal or not
+     */
+    @Override
+    public boolean equals(Object other) {
+        return super.equals(other) && other instanceof DeleteResponsePacket;
+    }
+
+    /**
+     * Translates the data of this packet into a readable format
+     *
+     * @return A formatted string
+     */
+    @Override
+    public String toString() {
+        return "DeleteResponsePacket{" +
+               "responseCode=" + getResponseStatus() + " (" + code + ")" +
+               "message=" + message +
+               '}';
     }
 }
