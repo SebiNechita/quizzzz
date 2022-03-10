0d2fa01983c4cac793d4ae2bbc7d8420126a6d97
Kristof Levay <levaykristof@student.tudelft.nl>
2022-03-10 16:46:15 +0100

    Remove unused import

--- client/src/main/java/client/scenes/RegisterCtrl.java
+++ client/src/main/java/client/scenes/RegisterCtrl.java
@@ -2,7 +2,6 @@ package client.scenes;
 
 import client.utils.ServerUtils;
 import com.google.inject.Inject;
-import commons.utils.LoggerUtil;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
