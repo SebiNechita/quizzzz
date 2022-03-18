0c28561e124006c7d5300aa3fedf13d5dee024b6
Mingyi Jin <m.jin-1@student.tudelft.nl>
2022-03-18 17:47:11 +0100

    add test for greetingController
    add test for greetingController

--- server/src/test/java/server/api/GreetingControllerTest.java
+++ server/src/test/java/server/api/GreetingControllerTest.java
@@ -4,10 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -20,17 +24,21 @@ public class GreetingControllerTest {
     @Autowired
     private MockMvc mvc;
 
-
+    /**
+     * test case for greet() method
+     * @throws Exception
+     */
     @Test
+    @WithMockUser(username = "Geoff")
     public void greetTest() throws Exception {
 
-        MvcResult res = mvc.perform(get("/api/greet")
+        MvcResult res = mvc.perform(get("/api/greet/Kim")
+                .secure( true )
                 .param("name", "Kim" )
-        ).andReturn();
+        ).andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
 
         String resStr = res.getResponse().getContentAsString();
-        System.out.println("=====================================3##########################"+resStr);
-        assertTrue(resStr.equals("Hi Kim!"));
+        assertTrue(resStr.equals("Hi, Kim!"));
 
     }
 }
