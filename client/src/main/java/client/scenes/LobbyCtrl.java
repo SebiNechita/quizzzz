c67d6ce6530aaf89f65b57da2fa3a864fad52b9c
Alexandru-Sebastian Nechita <A.Nechita@student.tudelft.nl>
2022-03-20 00:02:45 +0100

    Give functionalities to the emojis

--- client/src/main/java/client/scenes/LobbyCtrl.java
+++ client/src/main/java/client/scenes/LobbyCtrl.java
@@ -5,8 +5,10 @@ import client.utils.OnShowScene;
 import client.utils.ServerUtils;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -15,7 +17,7 @@ import javafx.scene.text.TextFlow;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class LobbyCtrl extends  SceneCtrl{
+public class LobbyCtrl extends SceneCtrl {
     @FXML
     private Button buttonReady;
     @FXML
@@ -26,7 +28,11 @@ public class LobbyCtrl extends  SceneCtrl{
     private Text playertext;
     @FXML
     private Text chattext;
+    @FXML
+    protected HBox emoteContainer;
+
     private Boolean ready;
+
     /**
      * Constructor for this Ctrl
      *
@@ -62,11 +68,193 @@ public class LobbyCtrl extends  SceneCtrl{
         playertext.setFill(Color.RED);
         playertext.setFont(Font.font("Comic Sans MS", 27));
         textflow.getChildren().add(playertext);
-        chattext = new Text("Quizzzz: Welcome to the game, " + Main.USERNAME + "\n");
+        chattext = new Text("Quizzzz: Welcome to the game, " + Main.USERNAME + "! " + "\n");
         chattext.setFont(Font.font("Comic Sans MS", 30));
         chattextflow.getChildren().add(chattext);
     }
 
+    /**
+     * add the Hearts Eye emoji into the chat
+     */
+    public void showEmoji1() {
+        Text text = new Text(Main.USERNAME + ": ");
+        text.setFont(Font.font("Comic Sans MS", 30));
+        Text text2 = new Text("\n");
+        ImageView iv = new ImageView("img/emojis/heart_eyes.png");
+        iv.setFitHeight(40);
+        iv.setFitWidth(40);
+        chattextflow.getChildren().addAll(text, iv, text2);
+        //chattextflow.getChildren().addAll(text, emoteContainer.getChildren().get(0),text2);
+    }
+
+    /**
+     * add the Joy emoji into the chat
+     */
+    public void showEmoji2() {
+        Text text = new Text(Main.USERNAME + ": ");
+        text.setFont(Font.font("Comic Sans MS", 30));
+        Text text2 = new Text("\n");
+        ImageView iv = new ImageView("img/emojis/joy.png");
+        iv.setFitHeight(40);
+        iv.setFitWidth(40);
+        chattextflow.getChildren().addAll(text, iv, text2);
+    }
+
+    /**
+     * add the Nerd emoji into the chat
+     */
+    public void showEmoji3() {
+        Text text = new Text(Main.USERNAME + ": ");
+        text.setFont(Font.font("Comic Sans MS", 30));
+        Text text2 = new Text("\n");
+        ImageView iv = new ImageView("img/emojis/nerd.png");
+        iv.setFitHeight(40);
+        iv.setFitWidth(40);
+        chattextflow.getChildren().addAll(text, iv, text2);
+    }
+
+    /**
+     * add the Smirking emoji into the chat
+     */
+    public void showEmoji4() {
+        Text text = new Text(Main.USERNAME + ": ");
+        text.setFont(Font.font("Comic Sans MS", 30));
+        Text text2 = new Text("\n");
+        ImageView iv = new ImageView("img/emojis/smirking.png");
+        iv.setFitHeight(40);
+        iv.setFitWidth(40);
+        chattextflow.getChildren().addAll(text, iv, text2);
+    }
+
+    /**
+     * add the Angry emoji into the chat
+     */
+    public void showEmoji5() {
+        Text text = new Text(Main.USERNAME + ": ");
+        text.setFont(Font.font("Comic Sans MS", 30));
+        Text text2 = new Text("\n");
+        ImageView iv = new ImageView("img/emojis/annoyed.png");
+        iv.setFitHeight(40);
+        iv.setFitWidth(40);
+        chattextflow.getChildren().addAll(text, iv, text2);
+    }
+
+    /**
+     * add the Sunglasses emoji into the chat
+     */
+    public void showEmoji6() {
+        Text text = new Text(Main.USERNAME + ": ");
+        text.setFont(Font.font("Comic Sans MS", 30));
+        Text text2 = new Text("\n");
+        ImageView iv = new ImageView("img/emojis/sunglasses.png");
+        iv.setFitHeight(40);
+        iv.setFitWidth(40);
+        chattextflow.getChildren().addAll(text, iv, text2);
+    }
+
+    /**
+     * add the Devil emoji into the chat
+     */
+    public void showEmoji7() {
+        Text text = new Text(Main.USERNAME + ": ");
+        text.setFont(Font.font("Comic Sans MS", 30));
+        Text text2 = new Text("\n");
+        ImageView iv = new ImageView("img/emojis/devil.png");
+        iv.setFitHeight(40);
+        iv.setFitWidth(40);
+        chattextflow.getChildren().addAll(text, iv, text2);
+    }
+
+    /**
+     * add the Sad emoji into the chat
+     */
+    public void showEmoji8() {
+        Text text = new Text(Main.USERNAME + ": ");
+        text.setFont(Font.font("Comic Sans MS", 30));
+        Text text2 = new Text("\n");
+        ImageView iv = new ImageView("img/emojis/sad.png");
+        iv.setFitHeight(40);
+        iv.setFitWidth(40);
+        chattextflow.getChildren().addAll(text, iv, text2);
+    }
+
+    /**
+     * add the Big Heart emoji into the chat
+     */
+    public void showEmoji9() {
+        Text text = new Text(Main.USERNAME + ": ");
+        text.setFont(Font.font("Comic Sans MS", 30));
+        Text text2 = new Text("\n");
+        ImageView iv = new ImageView("img/emojis/love.png");
+        iv.setFitHeight(40);
+        iv.setFitWidth(40);
+        chattextflow.getChildren().addAll(text, iv, text2);
+    }
+
+    /**
+     * add the Trophy emoji into the chat
+     */
+    public void showEmoji10() {
+        Text text = new Text(Main.USERNAME + ": ");
+        text.setFont(Font.font("Comic Sans MS", 30));
+        Text text2 = new Text("\n");
+        ImageView iv = new ImageView("img/emojis/trophy.png");
+        iv.setFitHeight(40);
+        iv.setFitWidth(40);
+        chattextflow.getChildren().addAll(text, iv, text2);
+    }
+
+    /**
+     * add the Thumbs up emoji into the chat
+     */
+    public void showEmoji11() {
+        Text text = new Text(Main.USERNAME + ": ");
+        text.setFont(Font.font("Comic Sans MS", 30));
+        Text text2 = new Text("\n");
+        ImageView iv = new ImageView("img/emojis/thumbup.png");
+        iv.setFitHeight(40);
+        iv.setFitWidth(40);
+        chattextflow.getChildren().addAll(text, iv, text2);
+    }
+
+    /**
+     * add the Exhaler emoji into the chat
+     */
+    public void showEmoji12() {
+        Text text = new Text(Main.USERNAME + ": ");
+        text.setFont(Font.font("Comic Sans MS", 30));
+        Text text2 = new Text("\n");
+        ImageView iv = new ImageView("img/emojis/exhaling.png");
+        iv.setFitHeight(40);
+        iv.setFitWidth(40);
+        chattextflow.getChildren().addAll(text, iv, text2);
+    }
+
+    /**
+     * add the Crying emoji into the chat
+     */
+    public void showEmoji13() {
+        Text text = new Text(Main.USERNAME + ": ");
+        text.setFont(Font.font("Comic Sans MS", 30));
+        Text text2 = new Text("\n");
+        ImageView iv = new ImageView("img/emojis/crying.png");
+        iv.setFitHeight(40);
+        iv.setFitWidth(40);
+        chattextflow.getChildren().addAll(text, iv, text2);
+    }
+
+    /**
+     * add the Thinker emoji into the chat
+     */
+    public void showEmoji14() {
+        Text text = new Text(Main.USERNAME + ": ");
+        text.setFont(Font.font("Comic Sans MS", 30));
+        Text text2 = new Text("\n");
+        ImageView iv = new ImageView("img/emojis/thinking.png");
+        iv.setFitHeight(40);
+        iv.setFitWidth(40);
+        chattextflow.getChildren().addAll(text, iv, text2);
+    }
 
 
     /**
@@ -83,13 +271,12 @@ public class LobbyCtrl extends  SceneCtrl{
     /**
      * When pressed it makes the player from the session ready or not ready.
      */
-    public void makeButtonReady(){
-        if(ready == false){
+    public void makeButtonReady() {
+        if (ready == false) {
             buttonReady.setBackground(new Background(new BackgroundFill(Color.GREEN, null, null)));
             playertext.setFill(Color.GREEN);
             ready = true;
-        }
-        else{
+        } else {
             buttonReady.setBackground(new Background(new BackgroundFill(Color.RED, null, null)));
             playertext.setFill(Color.RED);
             ready = false;
