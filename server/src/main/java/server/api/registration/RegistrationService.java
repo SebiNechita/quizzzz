f871ed4a9eba7ef7be3275908dba0eba0727bb91
Sander Vermeulen <S.R.Vermeulen@student.tudelft.nl>
2022-03-23 18:05:59 +0100

    Added JavaDoc for the registration endpoint

--- server/src/main/java/server/api/registration/RegistrationService.java
+++ server/src/main/java/server/api/registration/RegistrationService.java
@@ -15,10 +15,19 @@ public class RegistrationService {
 
     private final UserService appUserService;
 
+    /**
+     * @param appUserService Service which keeps track of the user database
+     */
     public RegistrationService(UserService appUserService) {
         this.appUserService = appUserService;
     }
 
+    /**
+     * Registers a user if it does not yet exist
+     *
+     * @param request Request packet containing the user details of the user who should be registered
+     * @return Response packet containing information if the registration was successful or not
+     */
     public RegisterResponsePacket register(RegisterRequestPacket request) {
         try {
             return appUserService.signUpUser(new User(request.getUsername(), request.getPassword(), false, true));
@@ -27,6 +36,12 @@ public class RegistrationService {
         }
     }
 
+    /**
+     * Checks whether a user is available or not
+     *
+     * @param username The username of the user to check the availability or not
+     * @return Response packet containing if a user is available or not
+     */
     public ResponsePacket userAvailable(String username) {
         if (appUserService.userExists(username)) {
             return new ResponsePacket(HttpStatus.Conflict);
@@ -35,6 +50,13 @@ public class RegistrationService {
         }
     }
 
+    /**
+     * Tries to delete a user specified by the received details. Only users themselves can delete their account.
+     *
+     * @param packet      Request packet containing the user details of the user who should be deleted
+     * @param authorities The authorities of the user who sent the request
+     * @return Response packet containing information if the deletion was successful or not
+     */
     public DeleteResponsePacket deleteUser(DeleteRequestPacket packet, Collection<? extends GrantedAuthority> authorities) {
         if (appUserService.userExists(packet.getUsername())) {
             if (appUserService.validUser(packet.getUsername(), packet.getPassword()) || authorities.stream().anyMatch(authority -> authority.getAuthority().equals("ADMIN"))) {
