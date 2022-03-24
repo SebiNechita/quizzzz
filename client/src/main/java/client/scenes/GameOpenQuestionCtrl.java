921e207adb586b96050f1e280015ba743d8d9700
Kaushik Karthikeyan <K.Karthikeyan-1@student.tudelft.nl>
2022-03-24 13:53:24 +0100

    Fixed an issue where the input field's radius changes once the timer ends
    Note: This commit only has 1 character change and I have no other changes. Shouldn't really be a commit but I did not know what to do :)

--- client/src/main/java/client/scenes/GameOpenQuestionCtrl.java
+++ client/src/main/java/client/scenes/GameOpenQuestionCtrl.java
@@ -234,7 +234,7 @@ public class GameOpenQuestionCtrl extends GameCtrl {
 
             @Override
             protected void interpolate(double frac) {
-                textField.setBackground(new Background(new BackgroundFill(lerp(start.getRed(), start.getGreen(), start.getBlue(), target.getRed(), target.getGreen(), target.getBlue(), frac), new CornerRadii(10), Insets.EMPTY)));
+                textField.setBackground(new Background(new BackgroundFill(lerp(start.getRed(), start.getGreen(), start.getBlue(), target.getRed(), target.getGreen(), target.getBlue(), frac), new CornerRadii(50), Insets.EMPTY)));
             }
         };
     }
