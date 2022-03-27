919a038009b5a28ab0ad76f0b8d7f90acf474b80
Kaushik Karthikeyan <K.Karthikeyan-1@student.tudelft.nl>
2022-03-27 19:09:59 +0200

    Ensures that user doesn't enter a URL that can break the code

--- client/src/main/java/client/scenes/ConnectionCtrl.java
+++ client/src/main/java/client/scenes/ConnectionCtrl.java
@@ -4,6 +4,7 @@ import client.utils.OnShowScene;
 import client.utils.ServerUtils;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
 import javafx.scene.text.Text;
 
 import java.net.URL;
@@ -49,6 +50,23 @@ public class ConnectionCtrl extends SceneCtrl {
     public void OnShowScene() {
         url.requestFocus();
         url.positionCaret(url.getText().length());
+        // Ensures that the user doesn't enter whitespaces in the url
+        TextFormatter<?> formatter = new TextFormatter<>((TextFormatter.Change change) -> {
+            String text = change.getText();
+
+            // if text was added, fix the text to fit the requirements
+            if (!text.isEmpty()) {
+                String newText = text.replaceAll("\\s|`|~|\\+|\\[|]|,|\"|;|}|\\{|'", "").toLowerCase();
+
+                int carretPos = change.getCaretPosition() - text.length() + newText.length();
+                change.setText(newText);
+
+                // fix carret position based on difference in originally added text and fixed text
+                change.selectRange(carretPos, carretPos);
+            }
+            return change;
+        });
+        url.setTextFormatter(formatter);
     }
 
     /**
