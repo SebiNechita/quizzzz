5b974f82f3ddda368b65870c543ba7288a0f36d0
Sander Vermeulen <S.R.Vermeulen@student.tudelft.nl>
2022-03-31 14:24:39 +0200

    Moved NotificationRenderer to separate class

--- client/src/main/java/client/scenes/GameCtrl.java
+++ client/src/main/java/client/scenes/GameCtrl.java
@@ -1,6 +1,7 @@
 package client.scenes;
 
 import client.Main;
+import client.utils.NotificationRenderer;
 import client.utils.ServerUtils;
 import commons.utils.Emote;
 import commons.utils.GameMode;
@@ -32,8 +33,6 @@ import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.util.Duration;
 
@@ -151,7 +150,7 @@ public abstract class GameCtrl extends SceneCtrl {
      * This method is called from its subclasses when that scene is displayed
      */
     public void onShowScene() {
-        notificationRenderer = new NotificationRenderer();
+        notificationRenderer = new NotificationRenderer(notificationContainer);
 
         timeLeftSlider = (AnchorPane) timeLeftBar.getChildren().get(0);
 
@@ -738,156 +737,5 @@ public abstract class GameCtrl extends SceneCtrl {
         media = new Media(soundFilePath);
         mediaPlayer = new MediaPlayer(media);
         mediaPlayer.play();
-    }
-
-    /**
-     * Utility class for rendering the notifications during the game.
-     */
-    public class NotificationRenderer {
-        private final Queue<AnchorPane> notifications = new LinkedList<>();
-        private Animation fadingOut = null;
-
-        /**
-         * Constructs an object and automatically clears out the notification panel.
-         */
-        private NotificationRenderer() {
-            notificationContainer.getChildren().clear();
-        }
-
-        /**
-         * Adds a notification for when a user uses an emote
-         *
-         * @param username The username who sent the emote
-         * @param emote    The emote which was sent
-         */
-        public void addEmoteNotification(String username, Emote emote) {
-            renderNotification(generateNotification(username + " reacted with:", new Color(1, 1, 1, 1), false, emote));
-        }
-
-        /**
-         * Adds a notification for when a user disconnects
-         *
-         * @param username The username who disconnected
-         */
-        public void addDisconnectNotification(String username) {
-            renderNotification(generateNotification(username + " has disconnected", new Color(0.949, 0.423, 0.392, 1), false));
-        }
-
-        /**
-         * Adds a notification for when a user uses a joker
-         *
-         * @param username The username who used the joker
-         * @param type     The type of joker used
-         */
-        public void addJokerNotification(String username, JokerType type) {
-            renderNotification(generateNotification(username + " has used a " + type.getName() + " joker!", new Color(0.541, 0.929, 1, 1), true));
-        }
-
-        /**
-         * Generates a notification
-         *
-         * @param text      The text to show in the notification
-         * @param textColor The color which the text should be
-         * @param bold      If the text should be bold or not
-         * @return The generated AnchorPane which contains the notification
-         */
-        private AnchorPane generateNotification(String text, Paint textColor, boolean bold) {
-            return generateNotification(text, textColor, bold, null);
-        }
-
-        /**
-         * Generates a notification
-         *
-         * @param text      The text to show in the notification
-         * @param textColor The color which the text should be
-         * @param bold      If the text should be bold or not
-         * @param emote     The emote that can be shown
-         * @return The generated AnchorPane which contains the notification
-         */
-        private AnchorPane generateNotification(String text, Paint textColor, boolean bold, Emote emote) {
-            AnchorPane anchorPane = new AnchorPane();
-            anchorPane.setPrefWidth(200);
-            anchorPane.setPrefHeight(80);
-            anchorPane.setBackground(new Background(new BackgroundFill(new Color(0.231, 0.231, 0.231, 0.8), new CornerRadii(8), Insets.EMPTY)));
-
-            Text title = new Text();
-            title.setLayoutX(6);
-            title.setLayoutY(22);
-            title.setWrappingWidth(200);
-
-            title.setText(text);
-            title.setFill(textColor);
-
-            title.setFont(Font.font("Comic Sans MS", bold ? FontWeight.BOLD : FontWeight.NORMAL, 18));
-
-            anchorPane.getChildren().add(title);
-
-            if (emote != null) {
-                ImageView image = new ImageView("@../../img/emojis/" + emote.toString().toLowerCase() + ".png");
-                image.setFitHeight(45);
-                image.setFitWidth(45);
-                image.setLayoutX(140);
-                image.setLayoutY(30);
-
-                ColorAdjust colorAdjust = new ColorAdjust();
-                colorAdjust.setBrightness(0);
-                image.setEffect(colorAdjust);
-
-                anchorPane.getChildren().add(image);
-            }
-
-            return anchorPane;
-        }
-
-        /**
-         * Renders a given notification in the form of an AnchorPane
-         *
-         * @param notification The AnchorPane to render
-         */
-        private void renderNotification(AnchorPane notification) {
-            ObservableList<Node> children = notificationContainer.getChildren();
-            children.add(notification);
-            notifications.add(notification);
-
-            if (notifications.size() > 3) {
-                if (fadingOut != null) {
-                    fadingOut.jumpTo(Duration.millis(600));
-                }
-
-                AnchorPane target = notifications.poll();
-                if (target != null) {
-                    fadingOut = fadeOut(target);
-                    fadingOut.setOnFinished(event -> {
-                        children.remove(target);
-                    });
-                    fadingOut.playFromStart();
-                }
-            }
-        }
-
-        /**
-         * Animates the notification to fade out
-         *
-         * @param anchorPane The AnchorPane to apply the animation to
-         * @return The animation object which can be played
-         */
-        private Animation fadeOut(AnchorPane anchorPane) {
-            return new Transition() {
-                {
-                    setCycleDuration(Duration.millis(600));
-                    setInterpolator(Interpolator.EASE_BOTH);
-                }
-
-                final Color color = (Color) anchorPane.getBackground().getFills().get(0).getFill();
-                final Text text = (Text) anchorPane.getChildren().get(0);
-                final Color textColor = (Color) text.getFill();
-
-                @Override
-                protected void interpolate(double frac) {
-                    anchorPane.setBackground(new Background(new BackgroundFill(new Color(color.getRed(), color.getGreen(), color.getBlue(), 1 - frac), new CornerRadii(10), Insets.EMPTY)));
-                    text.setFill(new Color(textColor.getRed(), textColor.getGreen(), textColor.getBlue(), 1 - frac));
-                }
-            };
-        }
     }
 }
